70fd2588eb2c930626afc41728fce5e5c49ed6f8
João Daniel <mr.joao200022@gmail.com>
2022-09-15 13:19:20 -0300

    Criei uma função para que valide somente a entrada de um valor positivo e maior que 0.

--- Teste.py
+++ Teste.py
@@ -7,51 +7,64 @@ import pendulum as pl
         # Fazer input e validação dos dados
 
 # Nome
+nome = str(input('Digite o seu nome: '))
+
+
+
 
 # Valor do investimento
+
+valor_investimento = fc.valida_valor(str(input('Digite o valor que deseja investir? R$ ')))
+
+
+
+
+
+
+
 
 # Data de Criação/ Validar datas
 
 print('Digite a data na sua aplição no formato: AAAA/MM/DD ')
 while True:
 
-    ano = fc.validanum(str(input('ano: ')))
+    ano = fc.valida_numero(str(input('ano: ')))
 
     while True:
         if ano > pl.today().year:
             print('\033[31m ERRO: A data não pode ser maior que a atual! \033[m')
-            ano = fc.validanum(str(input('Digite um ano válido: ')))
+            ano = fc.valida_numero(str(input('Digite um ano válido: ')))
         else:
             break
 
-    mes = fc.validanum(str(input('Mês: ')))
+    mes = fc.valida_numero(str(input('Mês: ')))
 
     if ano < pl.today().year:
         while True:
             if mes > 12 or mes < 0:
                 print(f'\033[31m ERRO: O mês {mes} não é válido! \033[m')
-                mes = fc.validanum(str(input('Digite um mês válido: ')))
+                mes = fc.valida_numero(str(input('Digite um mês válido: ')))
             else:
                 break
     else:
         while True:
             if mes > 12 or mes < 0:
                 print(f'\033[31m ERRO: O mês {mes} não é válido! \033[m')
-                mes = fc.validanum(str(input('Digite um mês válido: ')))
+                mes = fc.valida_numero(str(input('Digite um mês válido: ')))
             elif mes > pl.today().month:
                 print('\033[31m ERRO: A data não pode ser maior que a atual! \033[m')
-                mes = fc.validanum(str(input('Digite um mês válido: ')))
+                mes = fc.valida_numero(str(input('Digite um mês válido: ')))
             else:
                 break
 
-    dia = fc.validanum(str(input('Dia: ')))
+    dia = fc.valida_numero(str(input('Dia: ')))
 
     if ano < pl.today().year:
 
         while True:
             if dia < 0 or dia > 31:
                 print(f'\033[31m ERRO: O dia "{dia}" não é válido! \033[m')
-                dia = fc.validanum(str(input('Digite um dia válido: ')))
+                dia = fc.valida_numero(str(input('Digite um dia válido: ')))
             else:
                 break
 
@@ -60,17 +73,17 @@ while True:
             while True:
                 if dia < 0 or dia > 31:
                     print(f'\033[31m ERRO: O dia "{dia}" não é válido! \033[m')
-                    dia = fc.validanum(str(input('Digite um dia válido: ')))
+                    dia = fc.valida_numero(str(input('Digite um dia válido: ')))
                 else:
                     break
         else:
             while True:
                 if dia < 0 or dia > 31:
                     print(f'\033[31m ERRO: O dia "{dia}" não é válido! \033[m')
-                    dia = fc.validanum(str(input('Digite um dia válido: ')))
+                    dia = fc.valida_numero(str(input('Digite um dia válido: ')))
                 elif dia > pl.today().day:
                     print('\033[31m ERRO: A data não pode ser maior que a atual! \033[m')
-                    dia = fc.validanum(str(input('Digite um dia válido: ')))
+                    dia = fc.valida_numero(str(input('Digite um dia válido: ')))
                 else:
                     break
 
--- funcoes.py
+++ funcoes.py
@@ -1,12 +1,46 @@
 from datetime import date
 # Criar função para validar entrada de valores
 
+def valida_valor(n):
+
+    while True:
+
+        if ',' in n:
+            n.strip()
+            n.replace(',', '.')
+        else:
+            n.strip()
+
+        try:
+            num = float(n)
+        except ValueError:
+            print('\033[31m ERRO: O valor é inválido! \033[m')
+            n = str(input('Digite um valor válido: '))
+            continue
+
+        else:
+            if num < 0:
+                print('\033[31m ERRO: O valor não pode ser negativo! \033[m')
+                n = str(input('Digite um valor válido: '))
+                continue
+            elif num == 0:
+                print('\033[31m ERRO: o valor não pode ser igual a 0! \033[m')
+                n = str(input('Digite um valor válido: '))
+                continue
+            else:
+                return num
+                break
+
+
+
+
+
 
 
 
 # Criar função pra validar números inteiros
 
-def validanum(n):
+def valida_numero(n):
     while True:
         try:
             n = int(n)
@@ -21,5 +55,4 @@ def validanum(n):
 
 
 
-
-
+print(valida_valor(str(input())))
